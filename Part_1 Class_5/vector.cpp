1e437fea2d3ec1ec97bb2b3df835c0484b3d6c8b
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-12-16 23:56:00 +0600

    vector

---
+++ Part_1 Class_5/vector.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+    vector<int>::iterator it = v.begin();
+    cout << (*it) << '\n'; // Output: 1
+}
