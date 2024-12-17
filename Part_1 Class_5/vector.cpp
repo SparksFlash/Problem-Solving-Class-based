0d62bdf4b9023b8f0da263aa5bdc70bdaf528a4a
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-12-17 21:32:32 +0600

    push_back

--- Part_1 Class_5/vector.cpp
+++ Part_1 Class_5/vector.cpp
@@ -5,4 +5,8 @@ int main()
     vector<int> v = {1, 2, 3, 4, 5};
     vector<int>::iterator it = v.begin();
     cout << (*it) << '\n'; // Output: 1
+
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(30);
 }
