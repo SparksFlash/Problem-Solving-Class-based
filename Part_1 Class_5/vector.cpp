5b205a531b5b02d521ea8ba26093424b64c0b9fe
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-12-17 23:02:45 +0600

    push_back&Declaration

--- Part_1 Class_5/vector.cpp
+++ Part_1 Class_5/vector.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5};
-    vector<int>::iterator it = v.begin();
-    cout << (*it) << '\n'; // Output: 1
+    // vector<int> v = {1, 2, 3, 4, 5};
+    // vector<int>::iterator it = v.begin();
+    // cout << (*it) << '\n'; // Output: 1
 
-    vector<int> v;
+    vector<int> v(5);
     v.push_back(10);
     v.push_back(30);
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << ' ';
+    }
 }
