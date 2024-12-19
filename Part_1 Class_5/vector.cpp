b70c35d9a771e0da2c43daf885c5c15d2e5ca4ab
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-12-19 23:52:38 +0600

    vector-basic

--- Part_1 Class_5/vector.cpp
+++ Part_1 Class_5/vector.cpp
@@ -6,11 +6,30 @@ int main()
     // vector<int>::iterator it = v.begin();
     // cout << (*it) << '\n'; // Output: 1
 
-    vector<int> v{1, 2, 3, 4, 5};
+    vector<int> v;
     v.push_back(10);
     v.push_back(30);
+    v.push_back(30);
+    v.push_back(40);
+
+    v.resize(2);
+
+    v.assign(6, 5);
+    // vector<int> v(6,5);
+
+    // v.clear();
+
+    // v.pop_back();
+    // v.pop_back();
+    // v.pop_back();
+    // v.pop_back(); //error empty before pop
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << ' ';
     }
+
+    if (v.empty())
+        cout << "\nYES" << '\n';
+    else
+        cout << "\nNO" << '\n';
 }
