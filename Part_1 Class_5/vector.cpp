5391fa762342aae164c1c9d1caff95370820b4e4
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-12-19 23:39:59 +0600

    vector-learn

--- Part_1 Class_5/vector.cpp
+++ Part_1 Class_5/vector.cpp
@@ -6,7 +6,7 @@ int main()
     // vector<int>::iterator it = v.begin();
     // cout << (*it) << '\n'; // Output: 1
 
-    vector<int> v(5);
+    vector<int> v{1, 2, 3, 4, 5};
     v.push_back(10);
     v.push_back(30);
     for (int i = 0; i < v.size(); i++)
